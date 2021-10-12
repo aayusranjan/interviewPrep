5153c5c6dda42ba05b78d3998589ebcddd2d1acc
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2021-10-12 20:44:09 +0530

    Create checkCase.cpp

---
+++ Introduction to C++/Conditionals and Loops/checkCase.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char i;
+    cin >> i;
+    if (i >= 'a' && i <= 'z')
+    {
+        cout << "0";
+    }
+    else if (i >= 'A' && i <= 'Z')
+    {
+        cout << "1";
+    }
+    else
+    {
+        cout << "-1";
+    }
+}
