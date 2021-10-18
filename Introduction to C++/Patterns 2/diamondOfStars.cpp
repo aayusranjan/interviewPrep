826411305e5aa61c804a077d4da25ac412b686a4
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2021-10-18 21:44:26 +0530

    Create diamondOfStars.cpp

---
+++ Introduction to C++/Patterns 2/diamondOfStars.cpp
@@ -0,0 +1,27 @@
+/*
+Print the following pattern for the given number of rows.
+Note: N is always odd.
+
+Pattern for N = 5
+--*
+-***
+*****
+-***
+--*
+The dots represent spaces.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            
+        }
+    }
+}
