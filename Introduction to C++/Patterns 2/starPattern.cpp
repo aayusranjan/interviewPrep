6610f23098727c544da45cec0b1767f311e84bc0
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2021-10-18 21:46:40 +0530

    Create starPattern.cpp

---
+++ Introduction to C++/Patterns 2/starPattern.cpp
@@ -0,0 +1,41 @@
+/*
+Pattern for N = 4
+---*
+--***
+-*****
+*******
+The (-) represent spaces.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n - i)
+        {
+            cout << " ";
+            j++;
+        }
+        int k = 1;
+        while (k <= i)
+        {
+            cout << "*";
+            k++;
+        }
+        int l = 2;
+        while (l <= i)
+        {
+            cout << "*";
+            l++;
+        }
+        cout << endl;
+        i++;
+    }
+}
