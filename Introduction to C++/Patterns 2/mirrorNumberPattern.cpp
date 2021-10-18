5ba275b5c377ae413755786aa3f09c2676d4d504
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2021-10-18 21:46:06 +0530

    Create mirrorNumberPattern.cpp

---
+++ Introduction to C++/Patterns 2/mirrorNumberPattern.cpp
@@ -0,0 +1,37 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+---1
+--12
+-123
+1234
+
+the (-) represent spaces.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n - i)
+        {
+            cout << " ";
+            j++;
+        }
+        int k = 1;
+        while (k <= i)
+        {
+            cout << k;
+            k++;
+        }
+        cout << endl;
+        i++;
+    }
+}
