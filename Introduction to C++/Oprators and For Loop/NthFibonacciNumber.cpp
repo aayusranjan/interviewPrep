bbae7050fc2f18434d666c935267b127ea20b0f3
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2021-10-20 23:36:53 +0530

    Create NthFibonacciNumber.cpp

---
+++ Introduction to C++/Oprators and For Loop/NthFibonacciNumber.cpp
@@ -0,0 +1,24 @@
+/*
+Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+    F(n) = F(n-1) + F(n-2), 
+    Where, F(1) = F(2) = 1
+Provided N you have to find out the Nth Fibonacci Number.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a = 0;
+    int b = 1;
+    int c;
+    for (int i = 0; i < n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    cout << a;
+}
