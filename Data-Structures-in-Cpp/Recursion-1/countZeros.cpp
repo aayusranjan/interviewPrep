7527f37c800d7896a9a99ec24374a715da7178a4
AYUSH RANJAN <63737303+aayusranjan@users.noreply.github.com>
2022-01-25 19:24:51 +0530

    updated

--- Data-Structures-in-Cpp/Recursion-1/countZeros.cpp
+++ Data-Structures-in-Cpp/Recursion-1/countZeros.cpp
@@ -1,15 +1,20 @@
 /* input: 00010204
    output: 2 */
    
-int countZeros(int n) {
-    // Write your code here
-    if(n==0)
-        return 1;
-    int rem=n%10;
-    if(rem==0)
-        return 1+countZeros(n/10);
-    else 
-        return countZeros(n/10);
-
+int countZeros(int n){
+    if(n <= 9){
+        if(n = 0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+    int last = n % 10;
+    int count = 0;
+    if(last == 0){
+        count = 1;
+    }
+    return count + countZeros(n/10);
 }
 
